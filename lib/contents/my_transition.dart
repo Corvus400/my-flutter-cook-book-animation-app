a03580f61d6f4b0dc2fb3a854f0743b5fa8131ce
ryoma_tada <13657682+Corvus400@users.noreply.github.com>
2021-12-02 21:28:59 +0900

    :memo: Change 遷移処理を追加

--- lib/contents/my_transition.dart
+++ lib/contents/my_transition.dart
@@ -1,17 +1,42 @@
 import 'package:flutter/material.dart';
 
-class MyTransition extends StatefulWidget {
+class MyTransition extends StatelessWidget {
   const MyTransition({Key? key}) : super(key: key);
 
   @override
-  _MyTransitionState createState() => _MyTransitionState();
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.of(context).push(_createRoute());
+        },
+        child: const Text('Go!'),
+      ),
+    );
+  }
 }
 
-class _MyTransitionState extends State<MyTransition> {
+Route _createRoute() {
+  return PageRouteBuilder(
+    pageBuilder: (context, animation, secondaryAnimation) => const Page2(),
+    transitionsBuilder: (context, animation, secondaryAnimation, child) {
+      return child;
+    }
+  );
+}
+
+class Page2 extends StatelessWidget {
+  const Page2({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Animate a page route transition'),
+    return Center(
+      child: Scaffold(
+        appBar: AppBar(),
+        body: const Center(
+          child: Text('Page 2'),
+        ),
+      ),
     );
   }
 }
