f806ef8a51f65867a790fe0b487cb295b448011d
ryoma_tada <13657682+Corvus400@users.noreply.github.com>
2021-12-03 21:41:00 +0900

    :recycle: Update ボタンをタップすると色や形が変わる暗黙的アニメーションを追加

--- lib/contents/properties_container.dart
+++ lib/contents/properties_container.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 class PropertiesContainer extends StatefulWidget {
@@ -8,24 +10,54 @@ class PropertiesContainer extends StatefulWidget {
 }
 
 class _PropertiesContainerState extends State<PropertiesContainer> {
-  final double _width = 50;
-  final double _height = 50;
-  final Color _color = Colors.green;
-  final BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
+  double _width = 50;
+  double _height = 50;
+  Color _color = Colors.green;
+  BorderRadiusGeometry _borderRadius = BorderRadius.circular(8);
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: AnimatedContainer(
-        width: _width,
-        height: _height,
-        decoration: BoxDecoration(
-          color: _color,
-          borderRadius: _borderRadius,
+    return Stack(
+      children: [
+        Center(
+          child: AnimatedContainer(
+            width: _width,
+            height: _height,
+            decoration: BoxDecoration(
+              color: _color,
+              borderRadius: _borderRadius,
+            ),
+            duration: const Duration(seconds: 1),
+            curve: Curves.fastOutSlowIn,
+          ),
         ),
-        duration: const Duration(seconds: 1),
-        curve: Curves.fastOutSlowIn,
-      ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  final random = Random();
+
+                  _width = random.nextInt(300).toDouble();
+                  _height = random.nextInt(300).toDouble();
+
+                  _color = Color.fromRGBO(
+                    random.nextInt(256),
+                    random.nextInt(256),
+                    random.nextInt(256),
+                    1,
+                  );
+
+                  _borderRadius = BorderRadius.circular(random.nextInt(100).toDouble());
+                });
+              },
+              child: const Icon(Icons.play_arrow),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
