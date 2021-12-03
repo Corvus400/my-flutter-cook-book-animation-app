cc88d686436da9e52ff516acf431f4faba0ca95a
ryoma_tada <13657682+Corvus400@users.noreply.github.com>
2021-12-03 21:02:26 +0900

    :recycle: Update DraggableCardが指を離すと中央に戻るように変更

--- lib/contents/phisics_simulation.dart
+++ lib/contents/phisics_simulation.dart
@@ -27,24 +27,65 @@ class DraggableCard extends StatefulWidget {
   _DraggableCardState createState() => _DraggableCardState();
 }
 
-class _DraggableCardState extends State<DraggableCard> {
+class _DraggableCardState extends State<DraggableCard> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<Alignment> _animation;
+  Alignment _dragAlignment = Alignment.center;
+
   @override
   void initState() {
     super.initState();
+    _controller = AnimationController(vsync: this, duration: const Duration(seconds: 1));
+    _controller.addListener(() {
+      setState(() {
+        _dragAlignment = _animation.value;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return Align(
-      child: Card(
-        child: widget.child
+    var size = MediaQuery.of(context).size;
+    return GestureDetector(
+      onPanDown: (details) {
+        _controller.stop();
+      },
+      onPanUpdate: (details) {
+        setState(() {
+          _dragAlignment += Alignment(
+            details.delta.dx / (size.width / 2),
+            details.delta.dy / (size.height / 2),
+          );
+          print(_dragAlignment);
+        });
+      },
+      onPanEnd: (details) {
+        _runAnimation();
+      },
+      child: Align(
+        alignment: _dragAlignment,
+        child: Card(
+          child: widget.child
+        ),
       ),
     );
   }
 
   @override
   void dispose() {
+    _controller.dispose();
     super.dispose();
+  }
+
+  void _runAnimation() {
+    _animation = _controller.drive(
+      AlignmentTween(
+        begin: _dragAlignment,
+        end: Alignment.center,
+      ),
+    );
+    _controller.reset();
+    _controller.forward();
   }
 }
 
