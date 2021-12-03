adc672138d5a1b01e1bbdee22e4bcac96f933725
ryoma_tada <13657682+Corvus400@users.noreply.github.com>
2021-12-03 21:57:01 +0900

    :memo: Change コンテナをAnimatedOpacityで包んだ ボタンタップでフェードイン・フェードアウトするようにした

--- lib/contents/fade_widget.dart
+++ lib/contents/fade_widget.dart
@@ -8,14 +8,39 @@ class FadeWidget extends StatefulWidget {
 }
 
 class _FadeWidgetState extends State<FadeWidget> {
+  bool _visible = true;
+
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        width: 200.0,
-        height: 200.0,
-        color: Colors.green,
-      )
+    return Stack(
+      children: [
+        Center(
+            child: AnimatedOpacity(
+              opacity: _visible ? 1.0 : 0.0,
+              duration: const Duration(milliseconds: 500),
+              child: Container(
+                width: 200.0,
+                height: 200.0,
+                color: Colors.green,
+              ),
+            )
+        ),
+        Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Align(
+            alignment: Alignment.bottomRight,
+            child: FloatingActionButton(
+              onPressed: () {
+                setState(() {
+                  _visible = !_visible;
+                });
+              },
+              tooltip: 'Toggle Opacity',
+              child: const Icon(Icons.flip),
+            ),
+          ),
+        )
+      ],
     );
   }
 }
